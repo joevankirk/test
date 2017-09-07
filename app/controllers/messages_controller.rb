a424119a7e84e64c47a25527a8ba616b0f58de7d
Joe Van Kirk <imjoevankirk@gmail.com>
2017-09-07 17:36:53 -0500

    added CRUD logic to messages controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,26 @@
 class MessagesController < ApplicationController
+  before_action :set_chat, only:[:index, :create]
+  before_action :authenticate_user!
+
+  def index
+    @messages = @chat.messages.all
+    @messages = @messages.sort_by(&:created_at)
+  end
+
+  def create
+    @message = current_user.messages.build(message_params)
+    @message.save
+  end
+
+  private
+
+  def set_chat
+    @chow = Chat.find(params[:chat_id])
+  end
+
+
+  def message_params
+    params.require(:message).permit(:content, :sender_id, :recipient_id, :chat_id)
+  end
+
 end
